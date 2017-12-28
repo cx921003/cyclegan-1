7e01540883f3fab93b97f0ad2231805d680f9f9e
cx921003 <380231933@qq.com>
2017-12-28 18:00:25 +0100

    minor fix

--- creating_dataset.sh
+++ creating_dataset.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 python -m create_cyclegan_dataset \
-	--image_path_a ~/data/sketch2image/trainA \
-	--image_path_b ~/data/sketch2image/trainB \
-	--dataset_name="sketch2image_trainval_shuffled" \
+	--image_path_a ~/data/sketch2image_simplified/trainA \
+	--image_path_b ~/data/sketch2image_simplified/trainB \
+	--output_path ~/workspace/cyclegan-1/input/sketch2image_simplified/sketch2image_simplified_unshuffled.csv \
+	--do_shuffle=0
+#	--dataset_name="sketch2image_nonvis_unshuffled" \
+
+
+python -m create_cyclegan_dataset \
+	--image_path_a ~/data/sketch2image_simplified/trainA \
+	--image_path_b ~/data/sketch2image_simplified/trainB \
+	--output_path ~/workspace/cyclegan-1/input/sketch2image_simplified/sketch2image_simplified_shuffled.csv \
 	--do_shuffle=1
+#	--dataset_name="facades_train" \
+
+
+#python -m create_cyclegan_dataset \
+#        --image_path_a ~/data/cityscapes/trainA \
+#        --image_path_b ~/data/cityscapes/trainB \
+#        --dataset_name="cityscapes_train" \
+#        --do_shuffle=1
 
